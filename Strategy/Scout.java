c477ea90fb20a128195a5e30cb3336ebf43dcb83
i25959341 <karlson.lee09@gmail.com>
2016-02-25 14:32:46 +0000

    added dangourlocation

--- Strategy/Scout.java
+++ Strategy/Scout.java
@@ -22,6 +22,9 @@ public class Scout {
     public static Map<Integer, MapLocation> zombieDens = new HashMap<>();
     private static RobotController rc;
     private static Robot robot;
+    public static MapLocation dangerousLoc=null;
+    public static double lastHealth;
+    pulic static int turnWhileInDanger = 0;
     public static void init(RobotController _rc, Robot _robot) {
         robot = _robot;
         rc = _rc;
@@ -38,6 +41,20 @@ public class Scout {
                 zombieDens.put(z.ID, z.location);
             }
         }
+    }
+    
+    public static void dangerLocUpdate(){
+    	if(rc.getHealth()<lastHealth){
+    		dangerousLoc = rc.getLocation();
+    		turnWhileInDanger=0;
+    	}else if(dangerousLoc!= null){
+    		if (turnWhileInDanger>200){
+    			dangerousLoc=null;
+    			turnWhileInDanger = 0;
+    		}
+    		turnWhileInDanger++;
+    	}
+    	lastHealth = rc.getHealth();
     }
 
 }
